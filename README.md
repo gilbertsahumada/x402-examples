d1757f9cef97cc9c57bbf2abe9d73448a406b9ef
Gilberts Ahumada <gilbertsahumada@gmail.com>
2025-10-26 21:01:52 -0400

    updating readme file for better experience

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Instalar las dependencias 
 
 ```shell
-npm i @modelcontextprotocol/sdk axios dotenv viem x402-axios zod express
+npm i @modelcontextprotocol/sdk axios dotenv viem x402-axios zod express tsx x402-express
 ```
--- server.ts
+++ server.ts
@@ -1,12 +1,38 @@
-import express from 'express';
+import express from "express";
+import { config } from "dotenv";
+import { Hex } from "viem";
+import { paymentMiddleware, Resource } from "x402-express";
+
+config();
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello, World!');
+const facilitator = process.env.FACILITATOR as Resource;
+const payTo = process.env.PAY_TO as Hex;
+
+if (!facilitator || !payTo) {
+  throw new Error("Missing environment variables");
 }
+
+app.use(
+  paymentMiddleware(
+    payTo,
+    {
+      "GET /weather": {
+        price: 0.001,
+        network: "base-sepolia",
+      },
+    },
+    {
+      url: facilitator,
+    }
+  )
 );
+
+app.get("/weather", (_req, res) => {
+  res.send("Hello, World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
